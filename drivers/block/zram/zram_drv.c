2289b0334cdaf64ad528079d2878a5d695da017e
TianWalkzzMiku <whysdevsrk@gmail.com>
2023-01-14 03:53:10 +0000

    adreno/kgsl: Cleanup adrenokgsl_state includes

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -32,7 +32,6 @@
 #include <linux/idr.h>
 #include <linux/sysfs.h>
 #include <linux/debugfs.h>
-#include <linux/adrenokgsl_state.h>
 
 #include "zram_drv.h"
 
@@ -1498,9 +1497,6 @@ static ssize_t disksize_store(struct device *dev,
 	struct zram *zram = dev_to_zram(dev);
 	int err;
 
-#if defined(CONFIG_KERNEL_CUSTOM_E7S) || defined(CONFIG_KERNEL_CUSTOM_E7T)
-	true_gpu = false;
-#endif
 	disksize = memparse(buf, NULL);
 	if (!disksize)
 		return -EINVAL;
@@ -1557,9 +1553,6 @@ static ssize_t reset_store(struct device *dev,
 	if (!do_reset)
 		return -EINVAL;
 
-#if defined(CONFIG_KERNEL_CUSTOM_E7S) || defined(CONFIG_KERNEL_CUSTOM_E7T)
-	true_gpu = false;
-#endif
 	zram = dev_to_zram(dev);
 	bdev = bdget_disk(zram->disk, 0);
 	if (!bdev)
